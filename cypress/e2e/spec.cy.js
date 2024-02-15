e204b4d5099400dd5d7cefde8c2061baaad61b3e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-15 18:38:35 -0500

    p2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,26 @@
 /// <reference types="cypress" />
 
+// https://github.com/bahmutov/cypress-time-marks
+import 'cypress-time-marks'
+
 describe('Toast', () => {
   it(
-    'shows the expected message',
+    'shows the expected message for 2 seconds',
     { scrollBehavior: 'nearest' },
     () => {
       cy.visit('/toast').wait(1000)
-      cy.contains('basic-doc button', 'Show').click()
+      cy.contains('basic-doc button', 'Show')
+        .click()
+        .timeMark('clicked')
       cy.contains('p-toastitem', 'Message Content')
         .should('be.visible')
+        .timeSince('clicked', 'shows up', 100)
+        .timeMark('toast-shown')
         .find('.p-toast-message-success')
-      cy.get('p-toastitem').should('not.exist')
+      cy.get('p-toastitem')
+        .should('not.exist')
+        .timeSince('toast-shown', 'hides', 3500)
+      cy.log('**confirm timing**')
     },
   )
 })
