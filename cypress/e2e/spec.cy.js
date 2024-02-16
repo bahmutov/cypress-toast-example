85a86daa3d580196d7d059f2b2ade85b24492497
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-16 14:40:29 -0500

    initial p3

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,15 +5,23 @@ import 'cypress-time-marks'
 
 describe('Toast', () => {
   it(
-    'shows the expected message for 2 seconds',
+    'shows the expected message for N seconds',
     { scrollBehavior: 'nearest' },
     () => {
+      // can you refactor this test to confirm durations
+      // AFTER the functional test commands are over?
       cy.visit('/toast').wait(1000)
-      cy.contains('basic-doc button', 'Show').click()
+      cy.contains('basic-doc button', 'Show')
+        .click()
+        .timeMark('clicked')
       cy.contains('p-toastitem', 'Message Content')
         .should('be.visible')
+        .timeSince('clicked', 'shows up', 100)
+        .timeMark('toast-shown')
         .find('.p-toast-message-success')
-      cy.get('p-toastitem').should('not.exist')
+      cy.get('p-toastitem')
+        .should('not.exist')
+        .timeSince('toast-shown', 'hides', 3500)
       cy.log('**confirm timing**')
     },
   )
